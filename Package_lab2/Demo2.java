40c810e0063463bf3814c9151b6bd24fbf2adf99
sanchi285 <bismoy.sanchi@gmail.com>
2023-01-24 00:27:17 +0600

    Access Modifiers Practical Demo has been added

---
+++ Package_lab2/Demo2.java
@@ -0,0 +1,7 @@
+package mypack1;
+public class Demo2{
+	Demo1 d1 = new Demo1();
+	public void show(){
+		System.out.println(d1.a+d1.b+d1.c+d1.d);
+	}
+}
---
+++ Package_lab2/Demo3.java
@@ -0,0 +1,7 @@
+package mypack1;
+public class Demo3 extends Demo1{
+	Demo1 d1 = new Demo1();
+	public void show(){
+		System.out.println(d1.a+d1.b+d1.c+d1.d);
+	}
+}
