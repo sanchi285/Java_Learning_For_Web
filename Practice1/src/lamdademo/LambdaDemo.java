6ef52bdd5888aae65096449a870eb8da96b1c979
sanchi285 <bismoy.sanchi@gmail.com>
2023-02-08 23:24:30 +0600

    Parameters in lambda added

--- Practice1/src/lamdademo/LambdaDemo.java
+++ Practice1/src/lamdademo/LambdaDemo.java
@@ -4,14 +4,26 @@ package lamdademo;
 interface MyLambda {
     public void Display();
 }
+@FunctionalInterface
+interface Parameterized{
+    public int add(int a, int b);
+}
+
 public class LambdaDemo {
 
     public static void main(String[] args){
         MyLambda ml = ()->{
             System.out.println("Hello world");
         };
-
         ml.Display();
+
+        Parameterized pm = (a,b)->{return a+b;};
+        System.out.println(pm.add(23,50));
+
+        //anothe way
+        pm = (a, b) -> a*b;
+
+        System.out.println(pm.add(10,30));
 
     }
 }
