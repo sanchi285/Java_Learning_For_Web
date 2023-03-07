34fd2e527d087e144e76af475be767bb29e5a852
sanchi285 <bismoy.sanchi@gmail.com>
2023-03-07 23:50:17 +0600

    Object serialization completed for write operation

--- Practice1/src/javaiostream/SerializationDemo.java
+++ Practice1/src/javaiostream/SerializationDemo.java
@@ -1,10 +1,8 @@
 package javaiostream;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.io.*;
 
-class Student{
+class Student implements Serializable {
     String str;
     int rool;
     double cgpa;
@@ -15,15 +13,13 @@ public class SerializationDemo {
     public static void main(String [] args) throws Exception{
         String path1 = "/home/sanchi/learning/Java_Learning_web/Java_Learning_For_Web/Student1.txt";
         FileOutputStream fos = new FileOutputStream(path1);
-        PrintStream ps = new PrintStream(fos);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
         Student st  = new Student();
         st.str="Robi";
         st.rool=123;
         st.cgpa=3.21;
-        ps.println(st.str);
-        ps.println(st.rool);
-        ps.println(st.cgpa);
-        ps.close();
+        oos.writeObject(st);
+        oos.close();
         fos.close();
         System.out.println("Done Serialization");
     }
