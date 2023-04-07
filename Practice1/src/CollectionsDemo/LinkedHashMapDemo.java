108d8a3ae5b2885cdbd6648b216c892f6c8b74d8
sanchi285 <bismoy.sanchi@gmail.com>
2023-04-07 23:56:45 +0600

    Access based on order implemented

--- Practice1/src/CollectionsDemo/LinkedHashMapDemo.java
+++ Practice1/src/CollectionsDemo/LinkedHashMapDemo.java
@@ -3,13 +3,17 @@ import java.util.*;
 public class LinkedHashMapDemo {
     public static void main(String[] args){
         System.out.println("Linked Hash Map");
-        LinkedHashMap<Integer,String> lhm = new LinkedHashMap<>(5);
+        LinkedHashMap<Integer,String> lhm = new LinkedHashMap<>(5,.75f,true);
         lhm.put(1,"A");
         lhm.put(3,"H");
         lhm.put(5,"5");
         lhm.put(6,"H");
         lhm.put(7,"O");
         lhm.put(9,"P");
+        lhm.put(10,"M");
+
+        String s = lhm.get(1);
+        s = lhm.get(3);
 
         lhm.forEach((k,v)->System.out.println(k+" "+v));
     }
