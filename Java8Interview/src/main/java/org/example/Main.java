8161eb1f54f9b793635ac5a17e1bbf5c5dac5a84
sanchi285 <bismoy.sanchi@gmail.com>
2023-08-23 23:37:44 +0600

    Map group by added

--- Java8Interview/src/main/java/org/example/Main.java
+++ Java8Interview/src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -11,12 +13,30 @@ public class Main {
         map1.put("anil",890);
         map1.put("rahim",8976);
         map1.put("baller",900);
+        map1.put("rahim1",8976);
+        map1.put("baller1",900);
 
-      Map.Entry <String,Integer> res =   map1.entrySet().stream()
-                        .sorted(Map.Entry.comparingByValue())
-                                .collect(Collectors.toList())
-                                        .get(1);
+        //groupBy
+        Map<Integer, List<Map.Entry<String, Integer>>> map = map1.entrySet()
+                                                            .stream()
+                    .collect(Collectors.groupingBy(Map.Entry::getValue));
+
+        System.out.println(map);
+
+
+        getNthHeighstSalary(1,map1);
+    }
+
+    public static Map.Entry<String, Integer> getNthHeighstSalary(int num, Map<String, Integer> map){
+
+        Map.Entry <String,Integer> res =   map.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(Collectors.toList())
+                .get(num-1);
 
         System.out.println(res);
+        return res;
     }
+
+
 }
